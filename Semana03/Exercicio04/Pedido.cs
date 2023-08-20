cfbf8f32f200bea242a8b33ca499631b014e18e1
Erico <ericoam.mussulin5@gmail.com>
2023-08-20 10:58:04 -0300

    ajustes

--- Semana03/Exercicio04/Pedido.cs
+++ Semana03/Exercicio04/Pedido.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Semana03.Exercicio04
 {
     //Este código não está nos padrões Interface segregation principle, veja que tanto a classe PedidoEntrega quanto a PedidoBalcao são 
-    //obrigadas a implementar métodos que não são de seu real interesse.
+    //obrigadas a implementar métodos que não são de seu real interesse. A resolução do exerício está na pasta ResolucaoDoExercicio
     public class PedidoEntrega : IPedido
     {
         public void PedidoConfirmado()
